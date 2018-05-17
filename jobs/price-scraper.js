65e8e6e1de97962295092ae79dafc93260a410e8
Ben Hanna <benpaulhanna@gmail.com>
2018-05-17 17:10:35 +0000

    Implementation pipeline for transforming stock quote data.

--- jobs/price-scraper.js
+++ jobs/price-scraper.js
@@ -1,31 +1,44 @@
-const { chunk } = require('underscore');
-const { axios } = require('axios');
+const axios = require('axios');
+const _ = require('underscore');
 
+const { alphaVantage } = require('../secrets.json');
 const { Stock } = require('../lib/data/accessor');
 
-const CHUNK_SIZE = 25;
+const CHUNK_SIZE = 100;
 const API_URL = 'https://www.alphavantage.co/query';
 
-function async run() {
+async function run() {
     const stocks = await Stock().select();
-    const stockChunks = chunk(stocks, CHUNK_SIZE);
+    const stockChunks = _.chunk(stocks, CHUNK_SIZE);
 
     for (let stockChunk of stockChunks) {
         const quotes = await fetchQuotes(stockChunk);
+        console.log(quotes);
     }
 }
 
-function async fetchQuotes(stockChunk) {
+async function fetchQuotes(stockChunk) {
     const options = {
         params: {
-            apikey: process.env.API_KEY,
+            apikey: alphaVantage.apikey,
             function: 'BATCH_STOCK_QUOTES',
-            symbols: stockChunk.join(',')
+            symbols: stockChunk.map(stock => stock.symbol).join(',')
         }
     };
 
     const { data } = await axios.get(API_URL, options);
-    return data;
+
+    const quotePipeline = _.compose(
+        _.partial(_.map, _, x => {
+            return {
+                symbol: _.property('1. symbol')(x),
+                price: _.property('2. price')(x)
+            };
+        }),
+        _.property('Stock Quotes')
+    );
+
+    return quotePipeline(data);
 }
 
 try {
