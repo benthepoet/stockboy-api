99b21943e17517f545093d13a6c3824eae7a5917
Ben Hanna <benpaulhanna@gmail.com>
2018-05-28 23:41:01 +0000

    Fixed script errors.

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,57 +1,44 @@
--- user
-CREATE SEQUENCE user_id_sequence;
-
-SET TERM !! ;
-CREATE TRIGGER user_autoincrement FOR user
-ACTIVE BEFORE INSERT POSITION 0
-AS
-  IF (NEW.id IS NULL) THEN
-    NEW.id = NEXT VALUE FOR user_id_sequence
-BEGIN
-END
-SET TERM ; !!
-
-CREATE TABLE user (
-  id INTEGER PRIMARY NOT NULL,
+CREATE TABLE k_user (
+  id INTEGER PRIMARY KEY NOT NULL,
   email VARCHAR(255) UNIQUE,
   hash CHAR(60) NOT NULL,
   balance DECIMAL(10, 4)
 );
 
--- stock
-CREATE sequence stock_id_sequence;
+CREATE SEQUENCE user_id_seq;
 
 SET TERM !! ;
-CREATE TRIGGER stock_autoincrement FOR stock
+CREATE TRIGGER user_autoinc FOR k_user
 ACTIVE BEFORE INSERT POSITION 0
 AS
 BEGIN
   IF (NEW.id IS NULL) THEN
-    NEW.id = NEXT VALUE FOR stock_id_sequence
-END
+    NEW.id = NEXT VALUE FOR user_id_seq;
+END!!
 SET TERM ; !!
+
 
 CREATE TABLE stock (
   id INTEGER PRIMARY KEY NOT NULL,
   symbol VARCHAR(8) NOT NULL UNIQUE,
-  name VARHCAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
   sector VARCHAR(255) NOT NULL,
   last_price DECIMAL(10, 4),
   updated_at TIMESTAMP
 );
 
--- stock_position
-CREATE SEQUENCE stock_position_id_sequence;
+CREATE sequence stock_id_seq;
 
 SET TERM !! ;
-CREATE TRIGGER stock_position_autoincrement FOR stock_position
+CREATE TRIGGER stock_autoinc FOR stock
 ACTIVE BEFORE INSERT POSITION 0
 AS
 BEGIN
   IF (NEW.id IS NULL) THEN
-    NEW.id = NEXT VALUE FOR stock_position_id_sequence
-END
+    NEW.id = NEXT VALUE FOR stock_id_seq;
+END!!
 SET TERM ; !!
+
 
 CREATE TABLE stock_position (
   id INTEGER PRIMARY KEY NOT NULL,
@@ -62,27 +49,27 @@ CREATE TABLE stock_position (
 
 ALTER TABLE stock_position
 ADD CONSTRAINT fk_stock_position_stock_id
-FOREIGN KEY (stock_id) REFERENCES stock(id)
+FOREIGN KEY (stock_id) REFERENCES stock(id);
 
 ALTER TABLE stock_position
 ADD CONSTRAINT fk_stock_position_user_id
-FOREIGN KEY (user_id) REFERENCES user(id)
+FOREIGN KEY (user_id) REFERENCES k_user(id);
 
--- stock_position_order
-CREATE SEQUENCE stock_position_order_id_sequence;
+CREATE SEQUENCE stock_position_id_seq;
 
-SET TERM !!;
-CREATE TRIGGER stock_position_order_autoincrement FOR stock_position_order
+SET TERM !! ;
+CREATE TRIGGER stock_position_autoinc FOR stock_position
 ACTIVE BEFORE INSERT POSITION 0
 AS
 BEGIN
   IF (NEW.id IS NULL) THEN
-    NEW.id = NEXT VALUE FOR stock_position_order_id_sequence;
+    NEW.id = NEXT VALUE FOR stock_position_id_seq;
 END!!
 SET TERM ; !!
 
+
 CREATE TABLE stock_position_order (
-  id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY NOT NULL,
   units INTEGER NOT NULL,
   price DECIMAL(10, 4) NOT NULL,
   stock_position_id INTEGER NOT NULL
@@ -90,4 +77,16 @@ CREATE TABLE stock_position_order (
 
 ALTER TABLE stock_position_order
 ADD CONSTRAINT fk_stock_position_order_position_id
-FOREIGN KEY (stock_position_id) REFERENCES stock_position(id)
+FOREIGN KEY (stock_position_id) REFERENCES stock_position(id);
+
+CREATE SEQUENCE stock_position_order_id_seq;
+
+SET TERM !!;
+CREATE TRIGGER stock_position_order_autoinc FOR stock_position_order
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+  IF (NEW.id IS NULL) THEN
+    NEW.id = NEXT VALUE FOR stock_position_order_id_seq;
+END!!
+SET TERM ; !!
