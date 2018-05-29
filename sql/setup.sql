506655d06ad92c9d1927e89b5054c27606513ac8
Ben Hanna <benpaulhanna@gmail.com>
2018-05-29 03:27:42 +0000

    Renamed tables.

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,41 +1,41 @@
-CREATE TABLE k_user (
+CREATE TABLE end_user (
   id INTEGER PRIMARY KEY NOT NULL,
-  email VARCHAR(255) UNIQUE,
+  email VARCHAR(80) UNIQUE,
   hash CHAR(60) NOT NULL,
   balance DECIMAL(10, 4)
 );
 
-CREATE SEQUENCE user_id_seq;
+CREATE SEQUENCE seq_end_user_id;
 
 SET TERM !! ;
-CREATE TRIGGER user_autoinc FOR k_user
+CREATE TRIGGER end_user_bi FOR end_user
 ACTIVE BEFORE INSERT POSITION 0
 AS
 BEGIN
   IF (NEW.id IS NULL) THEN
-    NEW.id = NEXT VALUE FOR user_id_seq;
+    NEW.id = NEXT VALUE FOR seq_end_user_id;
 END!!
 SET TERM ; !!
 
 
 CREATE TABLE stock (
   id INTEGER PRIMARY KEY NOT NULL,
-  symbol VARCHAR(8) NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL,
-  sector VARCHAR(255) NOT NULL,
+  symbol VARCHAR(10) NOT NULL UNIQUE,
+  name VARCHAR(120) NOT NULL,
+  sector VARCHAR(120) NOT NULL,
   last_price DECIMAL(10, 4),
   updated_at TIMESTAMP
 );
 
-CREATE sequence stock_id_seq;
+CREATE sequence seq_stock_id;
 
 SET TERM !! ;
-CREATE TRIGGER stock_autoinc FOR stock
+CREATE TRIGGER stock_bi FOR stock
 ACTIVE BEFORE INSERT POSITION 0
 AS
 BEGIN
   IF (NEW.id IS NULL) THEN
-    NEW.id = NEXT VALUE FOR stock_id_seq;
+    NEW.id = NEXT VALUE FOR seq_stock_id;
 END!!
 SET TERM ; !!
 
@@ -53,40 +53,40 @@ FOREIGN KEY (stock_id) REFERENCES stock(id);
 
 ALTER TABLE stock_position
 ADD CONSTRAINT fk_stock_position_user_id
-FOREIGN KEY (user_id) REFERENCES k_user(id);
+FOREIGN KEY (user_id) REFERENCES end_user(id);
 
-CREATE SEQUENCE stock_position_id_seq;
+CREATE SEQUENCE seq_stock_position_id;
 
 SET TERM !! ;
-CREATE TRIGGER stock_position_autoinc FOR stock_position
+CREATE TRIGGER stock_position_bi FOR stock_position
 ACTIVE BEFORE INSERT POSITION 0
 AS
 BEGIN
   IF (NEW.id IS NULL) THEN
-    NEW.id = NEXT VALUE FOR stock_position_id_seq;
+    NEW.id = NEXT VALUE FOR seq_stock_position_id;
 END!!
 SET TERM ; !!
 
 
-CREATE TABLE stock_position_order (
+CREATE TABLE stock_order (
   id INTEGER PRIMARY KEY NOT NULL,
   units INTEGER NOT NULL,
   price DECIMAL(10, 4) NOT NULL,
   stock_position_id INTEGER NOT NULL
 );
 
-ALTER TABLE stock_position_order
-ADD CONSTRAINT fk_stock_position_order_position_id
+ALTER TABLE stock_order
+ADD CONSTRAINT fk_stock_order_position_id
 FOREIGN KEY (stock_position_id) REFERENCES stock_position(id);
 
-CREATE SEQUENCE stock_position_order_id_seq;
+CREATE SEQUENCE seq_stock_order_id;
 
 SET TERM !!;
-CREATE TRIGGER stock_position_order_autoinc FOR stock_position_order
+CREATE TRIGGER stock_order_bi FOR stock_order
 ACTIVE BEFORE INSERT POSITION 0
 AS
 BEGIN
   IF (NEW.id IS NULL) THEN
-    NEW.id = NEXT VALUE FOR stock_position_order_id_seq;
+    NEW.id = NEXT VALUE FOR seq_stock_order_id;
 END!!
 SET TERM ; !!
